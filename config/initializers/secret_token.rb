21bd2f81e2aa5a3e0a6f43cff7d29e69049e8db1
cheshireoctopus <chandlermoisen@gmail.com>
2013-09-23 14:48:30 -0400

    Inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spatialy::Application.config.secret_token = '6c8c7ef04ed512fb7faf0dfbed90131c770f37ca54bb11f70f5c2d1bc395cfcb0993eee08bbb515a272c090dfa5bd1af81e34ae98c3f80d975a791b3501bbe29'

1a765c4565e97079e378e3a2efaec1e5ae719d40
cheshireoctopus <chandlermoisen@gmail.com>
2014-03-12 13:29:28 -0400

    Cleaning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-def create
+  def create
     @user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = @user.id
     redirect_to home_path
